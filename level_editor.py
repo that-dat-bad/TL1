3a5d77fb055a6297e701ecc1f344264b8febdf7b
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2026-06-18 12:58:53 +0900

    07

--- level_editor.py
+++ level_editor.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import bpy  # type: ignore
+import bpy_extras
 import sys
 import io
 import os
 import math
+
 # Blenderのコンソール出力をUTF-8に設定（Windows文字化け対策）
 os.environ['PYTHONIOENCODING'] = 'utf-8'
 
@@ -32,7 +34,7 @@ try:
 except Exception:
     pass
 
-#ブレンダーに登録するアドオン情報
+# ブレンダーに登録するアドオン情報
 bl_info = {
     "name": "レベルエディタ",
     "author": "Daiki Takanaga",
@@ -46,120 +48,124 @@ bl_info = {
     "category": "Object"
 }
 
-#トップバーの拡張メニュー
+# トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
-    #Blenderがクラスを識別するための固有の文字列
     bl_idname = "TOPBAR_MT_my_menu"
-    #メニューのラベル
     bl_label = "MyMenu"
-    #著者表示用の文字列
     bl_description = "閣僚メニュー by" + bl_info["author"]
     
-    #サブメニューの描画
     def draw(self, context):
-       
-       self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
-        
-        # セパレーター(横線)
-       self.layout.separator()
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        self.layout.separator()
+        self.layout.operator(MYADDON_OT_strech_vertex.bl_idname, text=MYADDON_OT_strech_vertex.bl_label)
+        self.layout.separator()
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
+        self.layout.separator()
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
 
-       self.layout.operator(MYADDON_OT_strech_vertex.bl_idname, text=MYADDON_OT_strech_vertex.bl_label)
-       
-       self.layout.separator()
-
-       self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
-
-       self.layout.separator()
-
-       self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
-
-    #既存のメニューにサブメニューを追加
     def submenu(self, context):
-       
-       self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 
-
-#オペレータ 頂点を伸ばす
+# オペレータ 頂点を伸ばす
 class MYADDON_OT_strech_vertex(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_strech_vertex"
     bl_label = "頂点を伸ばす"
     bl_description = "選択した頂点を引っ張って伸ばします"
-    # redo undo可能オプション
     bl_options = {'REGISTER', 'UNDO'}
 
-    #メニューを実行したときに呼ばれるコールバック関数
     def execute(self, context):
         bpy.data.objects['Cube'].data.vertices[0].co.x += 1.0
         print("頂点を伸ばしました。")
-
-        #オペレーターの命令終了を通知
         return {'FINISHED'}
 
-#オペレータ ICO球生成
+
+# オペレータ ICO球生成
 class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_create_object"
     bl_label = "ICO球生成"
     bl_description = "ICO球を生成します"
-    # redo undo可能オプション
     bl_options = {'REGISTER', 'UNDO'}
 
-    #メニューを実行したときに呼ばれるコールバック関数
     def execute(self, context):
         bpy.ops.mesh.primitive_ico_sphere_add()
         print("ICO球を生成しました。")
-
-        #オペレーターの命令終了を通知
         return {'FINISHED'}
 
-#オペレータ シーン出力
-class MYADDON_OT_export_scene(bpy.types.Operator):
+
+# オペレータ シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
     bl_idname = "myaddon.myaddon_ot_export_scene"
     bl_label = "シーン出力"
     bl_description = "シーン情報をエクスポートします"
-   
+    filename_ext = ".scene"
 
     def execute(self, context):
         print("シーン情報をexportします")
-
-        #シーン内の全オブジェクトについて
-        for object in bpy.context.scene.objects:
-            print(object.type + " - " + object.name)
-            #ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
-            #型は Vector, Quaternion, Vector
-            trans, rot, scale = object.matrix_local.decompose()
-            #回転を Quaternion から Euler（3軸での回転角）に変換
-            rot = rot.to_euler()
-            #ラジアンから度数法に変換
-            rot.x = math.degrees(rot.x)
-            rot.y = math.degrees(rot.y)
-            rot.z = math.degrees(rot.z)
-            #トランスフォーム情報を表示
-            print("Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z) )
-            print("Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z) )
-            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z) )
-            #親オブジェクトの名前を表示
-            if object.parent:
-                print("Parent:" + object.parent.name)
-            print()
-
+        self.export()
         print("シーン情報をexportしました")
         self.report({'INFO'}, "シーン情報をexportしました")
-        
-        #オペレーターの命令終了を通知
         return {'FINISHED'}
 
-#メニュー項目描画
-def draw_menu_manual(self,context):
-    #self : 呼び出し元のクラスインスタンス C++でいうthis
-    #context : カーソルを合わせたときのポップアップのカスタマイズなどに使用
+    def parse_scene_recursive(self, file, object, level):
+        """シーン解析用再帰関数"""
+        
+        # 深さ分インデントする（タブを挿入）
+        indent = ''
+        for i in range(level):
+            indent += "\t"
 
-    #トップバーのエディタメニューに項目を追加
-    self.layout.operator("wm.url_open_preset",text ="Manual", icon ='HELP')
+        # オブジェクト名書き込み
+        self.write_and_print(file, indent + object.type + " - " + object.name)
+        
+        trans, rot, scale = object.matrix_local.decompose()
+        # 回転を Quternion から Euler （3軸での回転角）に変換
+        rot = rot.to_euler()
+        # ラジアンから度数法に変換
+        rot.x = math.degrees(rot.x)
+        rot.y = math.degrees(rot.y)
+        rot.z = math.degrees(rot.z)
+        
+        # トランスフォーム情報を表示
+        self.write_and_print(file, indent + "Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
+        self.write_and_print(file, indent + "Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
+        self.write_and_print(file, indent + "Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+        self.write_and_print(file, '')
+
+        # 子ノードへ進む（深さが1上がる）
+        for child in object.children:
+            self.parse_scene_recursive(file, child, level + 1)
+
+    def export(self):
+        """ファイル出力"""
+        print("シーン情報をファイルに出力...%r" % self.filepath)
+
+        # ファイルをテキスト形式で開く
+        with open(self.filepath, 'w', encoding='utf-8') as file:
+            file.write("SCENE\n")
+            
+            # シーン内の全オブジェクトについて
+            for object in bpy.context.scene.objects:
+                
+                # 親オブジェクトがあるものはスキップ（代わりに親から呼び出すから）
+                if (object.parent):
+                    continue
+                
+                # シーン直下のオブジェクトをルートノード（深さ0）とし、再帰関数で走査
+                self.parse_scene_recursive(file, object, 0)
+
+    def write_and_print(self, file, text_str):
+        print(text_str)
+        file.write(text_str)
+        file.write("\n")
 
 
+# メニュー項目描画
+def draw_menu_manual(self, context):
+    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
 
-#ブレンダに登録するクラスリスト
+
+# ブレンダに登録するクラスリスト
 classes = [
     TOPBAR_MT_my_menu,
     MYADDON_OT_strech_vertex,
@@ -168,27 +174,21 @@ classes = [
 ]
 
 
-#Add-On有効化時コールバック
+# Add-On有効化時コールバック
 def register():
-    #blenderにクラスを登録
     for cls in classes:
         bpy.utils.register_class(cls)
-    #メニューに項目を追加
     bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました。")
 
 
-#Add-On無効化時コールバック
+# Add-On無効化時コールバック
 def unregister():
-    #メニューから項目を削除
     bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
-    #blenderからクラスを削除
     for cls in classes:
         bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
     
-#テスト実行用
+# テスト実行用
 if __name__ == "__main__":
     register()
-
-
